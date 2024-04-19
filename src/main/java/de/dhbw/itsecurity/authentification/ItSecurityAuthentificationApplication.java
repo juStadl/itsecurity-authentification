0bfb073d4faebd027a328bf2c2d8cecd7dd39e7f
Julian Stadler <jstadler@testo.de>
2024-04-19 14:03:16 +0200

    inital commit

---
+++ src/main/java/de/dhbw/itsecurity/authentification/ItSecurityAuthentificationApplication.java
@@ -0,0 +1,13 @@
+package de.dhbw.itsecurity.authentification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItSecurityAuthentificationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItSecurityAuthentificationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/dhbw/itsecurity/authentification/ItSecurityAuthentificationApplicationTests.java
@@ -0,0 +1,13 @@
+package de.dhbw.itsecurity.authentification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItSecurityAuthentificationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
