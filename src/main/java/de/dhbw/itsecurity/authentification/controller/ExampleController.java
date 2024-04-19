e106b4177488caef4878dd4313bac04e03fdbaf5
Matteo Staar <matteo.staar@cjt.de>
2024-04-19 15:30:42 +0200

    token based login

--- src/main/java/de/dhbw/itsecurity/authentification/controller/ExampleController.java
+++ src/main/java/de/dhbw/itsecurity/authentification/controller/ExampleController.java
@@ -1,16 +1,19 @@
 package de.dhbw.itsecurity.authentification.controller;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 
+@RestController
+@SecurityRequirement(name = "bearerAuth")
 public class ExampleController {
 
 	
 	
 	@GetMapping("/example")
-	public String exampleResource() {
-		return "Example";
+	public String exampleResource(Authentication auth) {
+		return auth.getName();
 	}
 }
